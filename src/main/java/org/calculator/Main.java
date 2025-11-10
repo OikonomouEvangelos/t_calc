2ccf3d15ac92f2e6b3c374297674e121f31146e3
OikonomouEvangelos <oikonomouevaggelos35@gmail.com>
2025-11-10 23:14:41 +0200

    only division version !! Vaggelis

---
+++ src/main/java/org/calculator/Main.java
@@ -0,0 +1,57 @@
+package org.calculator;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Team Calculator started!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) { // Επανάληψη μέχρι να επιλέξει ο χρήστης να σταματήσει
+            double num1;
+            double num2;
+            char operator;
+            double result = 0;
+
+            System.out.print("Enter the first number: ");
+            num1 = scanner.nextDouble();
+
+            System.out.print("Enter an operator (+, -, *, /, ^, l for log): ");
+            operator = scanner.next().charAt(0);
+
+            // Έλεγχος έγκυρου τελεστή
+            while (operator != '+' && operator != '-' && operator != '*' &&
+                    operator != '/' && operator != '^' && operator != 'l') {
+                System.out.print("Invalid operator! Please enter one of (+, -, *, /, ^, l): ");
+                operator = scanner.next().charAt(0);
+            }
+
+            System.out.print("Enter the second number: ");
+            num2 = scanner.nextDouble();
+
+            switch (operator) {
+                case '/' -> {
+                    while (num2 == 0) {
+                        System.out.println("Error: Division by zero is not allowed!");
+                        System.out.print("Please enter a non-zero number: ");
+                        num2 = scanner.nextDouble();
+                    }
+                    result = num1 / num2;
+                }
+
+            }
+
+            System.out.println("Result: " + result);
+
+            System.out.print("Do you want to perform another calculation? (y/n): ");
+            char choice = scanner.next().charAt(0);
+            if (choice == 'n' || choice == 'N') {
+                System.out.println("Exiting calculator. Goodbye!");
+                break;
+            }
+        }
+
+        scanner.close();
+    }
+}
